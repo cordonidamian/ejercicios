898b1d42ed954274b41916baaad6c9fad228416d
fvgtekken <federicovongromann@gmail.com>
2020-02-24 19:43:09 -0300

    Solution Repeat Elements

--- ejercicios/2/script.js
+++ ejercicios/2/script.js
@@ -1,13 +1,77 @@
-sumaArray = elemArray => {
-  return elemArray.reduce((a, b) => a + b);
+duplicatedEleArray = elemArray => {
+  const count = {};
+  const uniqueCount = [];
+
+  elemArray.forEach(ele => {
+    count[ele] = (count[ele] || 0) + 1;
+  });
+
+  Object.keys(count).forEach(key => {
+    let number = count[key];
+    uniqueCount.push(number);
+  });
+
+  uniqueCount.sort((a, b) => {
+    return b - a;
+  });
+
+  const maxNumber = uniqueCount[0];
+
+  const OrderCount = Object.keys(count).filter(key => {
+    return count[key] === maxNumber;
+  });
+
+  return OrderCount;
 };
 
-let elemArray = [];
+let elemArray = [
+  "5",
+  "5",
+  "5",
+  "5",
+  "7",
+  "8",
+  "8",
+  "9",
+  "9",
+  "10",
+  "4",
+  "4",
+  "4",
+  "4",
+  "4",
+  "4",
+  "5",
+  "5"
+];
 
-let limiteArray = 10;
+let elemArray2 = [
+  "a",
+  "a",
+  "a",
+  "a",
+  "b",
+  "c",
+  "f",
+  "f",
+  "g",
+  "g",
+  "v",
+  "v",
+  "v",
+  "v",
+  "v",
+  "v",
+  "a",
+  "a"
+];
 
-for (let i = 0; i < limiteArray; i++) {
-  elemArray.push(i);
-}
+console.log(
+  "Los Elementos que mas Se Repiten Son:",
+  this.duplicatedEleArray(elemArray)
+);
 
-console.log(this.sumaArray(elemArray));
+console.log(
+  "Los Elementos que mas Se Repiten Son:",
+  this.duplicatedEleArray(elemArray2)
+);
